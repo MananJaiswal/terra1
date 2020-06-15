2236435a81fbb392b16fe04c6a46d405c0e18a2f
MananJaiswal <64462540+MananJaiswal@users.noreply.github.com>
2020-06-15 10:51:47 +0530

    first1

---
+++ index.php
@@ -0,0 +1,12 @@
+<body bgcolor="red">
+<pre>
+<?php
+print `ifconfig`;
+echo ' 
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<img src="terraform.png" width="500" height="300" title="Terraform" alt="Terraform" />
+</html>
+';
+?>
+</pre>
